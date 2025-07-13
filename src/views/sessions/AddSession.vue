54f5a9b17c3c9245e3d04fa0b7c806cfb5cb96e2
John Knotts <jhnnyk@gmail.com>
2025-07-13 14:04:15 -0600

    add date picker

--- src/views/sessions/AddSession.vue
+++ src/views/sessions/AddSession.vue
@@ -1,7 +1,11 @@
 <script setup>
+import { ref } from 'vue'
 import { useSkateparkStore } from '@/stores/SkateparkStore'
+import VueDatePicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
 
 const skateparkStore = useSkateparkStore()
+const date = ref()
 </script>
 
 <template>
@@ -10,7 +14,13 @@ const skateparkStore = useSkateparkStore()
     <section>
       <form>
         <h1>Add {{ skateparkStore.getCurrentPark.name }} Session</h1>
+        <div class="field">
+          <label for="date">date</label><br />
+          <VueDatePicker v-model="date" :month-change-on-scroll="false" />
+        </div>
       </form>
     </section>
   </div>
 </template>
+
+<style></style>
