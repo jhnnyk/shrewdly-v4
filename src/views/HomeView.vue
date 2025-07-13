cba623744a7b6caeb6a709896e9c74a592444f68
John Knotts <jhnnyk@gmail.com>
2025-07-13 09:04:54 -0600

    basic skatepark listing on homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,16 @@ const skateparkStore = useSkateparkStore()
 </script>
 
 <template>
-  <section v-for="park in skateparkStore.getParks" :key="park.id">
-    <h1>{{ park.name }}</h1>
+  <section v-for="park in skateparkStore.getParks" :key="park.id" class="listing">
+    <h3>{{ park.name }}</h3>
+    <p>
+      <span class="material-symbols-outlined"> location_on </span>
+      {{ park.city }}, {{ park.state.substring(0, 2) }}
+    </p>
+
+    <div class="stats">
+      <p><span class="material-symbols-outlined"> photo_camera </span> 0 photos</p>
+      <p><span class="material-symbols-outlined"> group </span> 0 visits</p>
+    </div>
   </section>
 </template>
