e55433bb5ae1d29dcdd6dbc104a6afc1376a3715
John Knotts <jhnnyk@gmail.com>
2025-07-18 07:48:41 -0600

    ImageUploader component

---
+++ src/components/ImageUploader.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+import { useUploadsStore } from '@/stores/UploadsStore'
+
+const uploadsStore = useUploadsStore()
+const selectedFile = ref(null)
+
+const handleFileSelect = (event) => {
+  selectedFile.value = event.target.files[0]
+}
+
+const uploadImage = () => {
+  if (selectedFile.value) {
+    uploadsStore.uploadImage(selectedFile.value)
+  }
+}
+</script>
+
+<template>
+  <div>
+    <input type="file" @change="handleFileSelect" accept="image/*" />
+    <button @click="uploadImage" :disabled="!selectedFile || uploadsStore.isUploading">
+      upload
+    </button>
+
+    <div v-if="uploadsStore.isUploading">
+      uploading: {{ uploadsStore.uploadProgress.toFixed(2) }}
+    </div>
+
+    <div v-if="uploadsStore.uploadError">error: {{ uploadsStore.uploadError.message }}</div>
+
+    <div v-if="uploadsStore.uploadedImageUrl">
+      <p>image uploaded and resized!</p>
+      <img :src="uploadsStore.uploadedImageUrl" alt="uploaded image" />
+    </div>
+  </div>
+</template>
